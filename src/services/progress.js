ea501837d4e36da308ca7aef4081ca3994e2bad5
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2020-10-13 10:23:21 +0300

    Julkaise osa 7

--- src/services/progress.js
+++ src/services/progress.js
@@ -44,7 +44,6 @@ export async function fetchProgress(t) {
     }
   })
   // TODO: this is not a good way to do this
-  toBeDeleted.push("osa07")
   toBeDeleted.push("osa08")
   toBeDeleted.push("osa09")
   toBeDeleted.push("osa10")
