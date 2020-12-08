24d2b0a7415a99e95d7e640f1ee7fcccb4972937
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2020-12-08 12:16:40 +0200

    Use correct course and org

--- course-settings.js
+++ course-settings.js
@@ -5,9 +5,9 @@ const courseSettings = {
   githubUrl: "https://github.com/rage/python-s20",
   subtitle: "Johdatus ohjelmointiin Python-kielellä",
   slug: "python-s20",
-  tmcCourse: "python-test",
+  tmcCourse: "python-s20",
   quizzesId: "a8c6418a-98e4-4732-b09f-bd866da480d4",
-  tmcOrganization: "test",
+  tmcOrganization: "hy",
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
 }
