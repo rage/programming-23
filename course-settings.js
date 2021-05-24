a79547c974c1d102c8e60c26ea4c2a715456c231
Sebastian Sergelius <sebastian.sergelius@helsinki.fi>
2021-05-24 09:51:34 +0300

    Update course settings

--- course-settings.js
+++ course-settings.js
@@ -1,47 +1,22 @@
 const courseSettings = {
-  language: "fi",
-  name: "Ohjelmoinnin perusteet ja jatkokurssi 2021",
-  siteUrl: "https://ohjelmointi-21.mooc.fi",
-  githubUrl: "https://github.com/rage/ohjelmointi-21",
-  subtitle: "Johdatus ohjelmointiin Python-kielellä",
-  slug: "ohjelmointi-21",
+  language: "en",
+  name: "Python Programming MOOC 2021",
+  siteUrl: "https://programming-21.mooc.fi",
+  githubUrl: "https://github.com/rage/programming-21",
+  subtitle: "Python Programming MOOC",
+  slug: "programming-21",
   organizationName: "MOOC",
-  tmcCourse: "ohjelmointi-21",
-  quizzesId: "644ecb1d-becf-4e68-aeb4-8563b6d366c6",
+  tmcCourse: "programming-21",
+  quizzesId: "7bbf042f-25dc-4c79-8529-f0193857fd89",
   tmcOrganization: "mooc",
   bannerPath: "banner.svg",
   showExerciseDescriptionWhenNotLoggedIn: true,
   courseVariants: [
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT21A",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT21B",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT21C",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT21D",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT21E",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
-    {
-      tmcOrganization: "hbc",
-      tmcCourse: "python-TVT21F",
-      // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
-    },
+    // {
+    //   tmcOrganization: "hbc",
+    //   tmcCourse: "python-TVT21A",
+    //   // quizzesId: "5c89b9b6-b8a6-4079-8c4f-a4bbc80b66a4",
+    // },
   ],
 }
 
